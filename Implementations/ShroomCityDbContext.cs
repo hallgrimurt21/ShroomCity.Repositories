7d6f483506a02cc389d06f2eb936ae588c19e645
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-19 20:20:24 +0000

    Update  ShroomCityDbContext.cs for entities and seeding

--- Implementations/ShroomCityDbContext.cs
+++ Implementations/ShroomCityDbContext.cs
@@ -1,15 +1,59 @@
 namespace ShroomCity.Repositories.Implementations;
 using Microsoft.EntityFrameworkCore;
-using ShroomCity.Models;
+using ShroomCity.Models.Entities;
+using ShroomCity.Repositories.Seed;
 
-{
-    public class ShroomCityDbContext : DbContext
+public class ShroomCityDbContext : DbContext
 {
     public ShroomCityDbContext(DbContextOptions<ShroomCityDbContext> options) : base(options)
     {
+
     }
 
-    // Define your DbSets here. For example:
-    // public DbSet<Shroom> Shrooms { get; set; }
+    public DbSet<Attribute> Attributes { get; set; }
+    public DbSet<AttributeType> AttributeTypes { get; set; }
+    public DbSet<JwtToken> JwtTokens { get; set; }
+    public DbSet<Mushroom> Mushrooms { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Attribute>()
+            .HasKey(a => a.Id);
+        // Foreign key to AttributeType
+        modelBuilder.Entity<Attribute>()
+            .HasOne(a => a.AttributeType)
+            .WithOne(at => at.Attribute)
+            .HasForeignKey<Attribute>(a => a.AttributeTypeId);
+        // Foreign key to User
+        modelBuilder.Entity<Attribute>()
+            .HasOne(a => a.RegisteredBy)
+            .WithMany()
+            .HasForeignKey(a => a.RegisteredById);
+
+        modelBuilder.Entity<AttributeType>()
+            .HasKey(a => a.Id);
+        modelBuilder.Entity<User>()
+            .HasKey(a => a.Id);
+
+        modelBuilder.Entity<JwtToken>()
+            .HasKey(a => a.Id);
+
+        modelBuilder.Entity<Mushroom>()
+            .HasKey(a => a.Id);
+
+        modelBuilder.Entity<Permission>()
+            .HasKey(a => a.Id);
+
+        modelBuilder.Entity<Role>()
+            .HasKey(a => a.Id);
+
+        modelBuilder.Entity<User>()
+            .HasKey(a => a.Id);
+
+        modelBuilder.Seed();
+    }
 }
-}
+
+
--- Seed/ModelBuilderExtensions.cs
+++ Seed/ModelBuilderExtensions.cs
@@ -1,12 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using ShroomCity.Models.Constants;
+using ShroomCity.Models.Entities;
 using ShroomCity.Models.Enums;
 
 namespace ShroomCity.Repositories.Seed;
 
 public static class ModelBuilderExtensions
 {
+#pragma warning disable IDE1006 // Naming Styles
     private static readonly List<string> _attributeTypes = new()
+#pragma warning restore IDE1006 // Naming Styles
     {
         AttributeTypeEnum.Color.ToString(),
         AttributeTypeEnum.Shape.ToString(),
@@ -15,7 +18,9 @@ public static class ModelBuilderExtensions
         AttributeTypeEnum.StemSize.ToString()
     };
 
+#pragma warning disable IDE1006 // Naming Styles
     private static readonly List<string> _roles = new()
+#pragma warning restore IDE1006 // Naming Styles
     {
         RoleConstants.Admin,
         RoleConstants.Researcher,
@@ -24,75 +29,74 @@ public static class ModelBuilderExtensions
 
     public static void Seed(this ModelBuilder modelBuilder)
     {
-        // TODO: Uncomment for data seeding.
 
-        // modelBuilder.Entity<AttributeType>().HasData(
-        //     _attributeTypes.Select((a, idx) => new AttributeType
-        //     {
-        //         Id = idx + 1,
-        //         Type = a
-        //     })
-        // );
+        modelBuilder.Entity<AttributeType>().HasData(
+            _attributeTypes.Select((a, idx) => new AttributeType
+            {
+                Id = idx + 1,
+                Type = a
+            })
+        );
 
-        // modelBuilder.Entity<Role>()
-        //     .HasData(_roles.Select((r, idx) => new Role
-        //     {
-        //         Id = idx + 1,
-        //         Name = r
-        //     }));
+        modelBuilder.Entity<Role>()
+            .HasData(_roles.Select((r, idx) => new Role
+            {
+                Id = idx + 1,
+                Name = r
+            }));
 
-        // modelBuilder.Entity<Permission>()
-        //     .HasData(
-        //         new Permission
-        //         {
-        //             Id = 1,
-        //             Code = PermissionConstants.ReadMushrooms,
-        //             Description = "Allows users to read mushroom data."
-        //         },
-        //         new Permission
-        //         {
-        //             Id = 2,
-        //             Code = PermissionConstants.WriteMushrooms,
-        //             Description = "Allows users to write mushroom data."
-        //         },
-        //         new Permission
-        //         {
-        //             Id = 3,
-        //             Code = PermissionConstants.ReadResearchers,
-        //             Description = "Allows users to read researcher data."
-        //         },
-        //         new Permission
-        //         {
-        //             Id = 4,
-        //             Code = PermissionConstants.WriteResearchers,
-        //             Description = "Allows users to write researcher data."
-        //         }
-        //     );
+        modelBuilder.Entity<Permission>()
+            .HasData(
+                new Permission
+                {
+                    Id = 1,
+                    Code = PermissionConstants.ReadMushrooms,
+                    Description = "Allows users to read mushroom data."
+                },
+                new Permission
+                {
+                    Id = 2,
+                    Code = PermissionConstants.WriteMushrooms,
+                    Description = "Allows users to write mushroom data."
+                },
+                new Permission
+                {
+                    Id = 3,
+                    Code = PermissionConstants.ReadResearchers,
+                    Description = "Allows users to read researcher data."
+                },
+                new Permission
+                {
+                    Id = 4,
+                    Code = PermissionConstants.WriteResearchers,
+                    Description = "Allows users to write researcher data."
+                }
+            );
 
-        // modelBuilder.Entity<Role>()
-        //     .HasMany(r => r.Permissions)
-        //     .WithMany(p => p.Roles)
-        //     .UsingEntity<Dictionary<string, object>>(
-        //         "PermissionRole",
-        //         r => r.HasOne<Permission>().WithMany().HasForeignKey("PermissionId"),
-        //         p => p.HasOne<Role>().WithMany().HasForeignKey("RoleId"),
-        //         rp =>
-        //         {
-        //             rp.HasKey("RoleId", "PermissionId");
-        //             rp.HasData(
-        //                 // Admin role
-        //                 new { RoleId = 1, PermissionId = 1 },
-        //                 new { RoleId = 1, PermissionId = 2 },
-        //                 new { RoleId = 1, PermissionId = 3 },
-        //                 new { RoleId = 1, PermissionId = 4 },
-        //                 // Researcher role
-        //                 new { RoleId = 2, PermissionId = 1 },
-        //                 new { RoleId = 2, PermissionId = 2 },
-        //                 new { RoleId = 2, PermissionId = 3 },
-        //                 // Analyst role
-        //                 new { RoleId = 3, PermissionId = 1 },
-        //                 new { RoleId = 3, PermissionId = 3 });
-        //         }
-        //     );
+        modelBuilder.Entity<Role>()
+            .HasMany(r => r.Permissions)
+            .WithMany(p => p.Roles)
+            .UsingEntity<Dictionary<string, object>>(
+                "PermissionRole",
+                r => r.HasOne<Permission>().WithMany().HasForeignKey("PermissionId"),
+                p => p.HasOne<Role>().WithMany().HasForeignKey("RoleId"),
+                rp =>
+                {
+                    rp.HasKey("RoleId", "PermissionId");
+                    rp.HasData(
+                        // Admin role
+                        new { RoleId = 1, PermissionId = 1 },
+                        new { RoleId = 1, PermissionId = 2 },
+                        new { RoleId = 1, PermissionId = 3 },
+                        new { RoleId = 1, PermissionId = 4 },
+                        // Researcher role
+                        new { RoleId = 2, PermissionId = 1 },
+                        new { RoleId = 2, PermissionId = 2 },
+                        new { RoleId = 2, PermissionId = 3 },
+                        // Analyst role
+                        new { RoleId = 3, PermissionId = 1 },
+                        new { RoleId = 3, PermissionId = 3 });
+                }
+            );
     }
 }
