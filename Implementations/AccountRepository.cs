00cafc5957a5e171a59df735004fa95a044f7466
hallgrimurt21 <hallgrimurjth@gmail.com>
2023-12-20 15:21:06 +0000

    Add Register method to AccountRepository

--- Implementations/AccountRepository.cs
+++ Implementations/AccountRepository.cs
@@ -1,14 +1,42 @@
+namespace ShroomCity.Repositories.Implementations;
+
+using Microsoft.EntityFrameworkCore;
 using ShroomCity.Models.Dtos;
+using ShroomCity.Models.Entities;
 using ShroomCity.Models.InputModels;
 using ShroomCity.Repositories.Interfaces;
 
-namespace ShroomCity.Repositories.Implementations;
-
 public class AccountRepository : IAccountRepository
 {
-    public Task<UserDto?> Register(RegisterInputModel inputModel)
+    private readonly ShroomCityDbContext context;
+
+    public AccountRepository(ShroomCityDbContext context) => this.context = context;
+
+    public async Task<UserDto?> Register(RegisterInputModel inputModel)
     {
-        throw new NotImplementedException();
+        var existingUser = await this.context.Users
+            .FirstOrDefaultAsync(u => u.EmailAddress == inputModel.EmailAddress);
+        if (existingUser != null)
+        {
+            return null;
+        }
+        var analystRole = await this.context.Roles
+            .FirstOrDefaultAsync(r => r.Name == "Analyst") ?? throw new Exception("Role Analyst not found");
+
+        var permissions = analystRole.Permissions.Select(p => p.Code).ToList();
+        var token = "Todo: generate token", /// TODO
+        var newUser = new User
+        {
+            Name = inputModel.FullName,
+            EmailAddress = inputModel.EmailAddress,
+            HashedPassword = inputModel.Password,
+            Bio = inputModel.Bio,
+            Role = analystRole,
+            RegisterationDate = DateTime.Now
+        };
+        this.context.Users.Add(newUser);
+        await this.context.SaveChangesAsync();
+        return new UserDto { Name = newUser.Name, EmailAddress = newUser.EmailAddress, Permissions = permissions, TokenId = token };
     }
 
     public Task<UserDto?> SignIn(LoginInputModel inputModel)
