384f586c08a49d650c51ddf4cf358926df96f6d2
hallgrimurt21 <hallgrimurth@gmail.com>
2023-12-26 20:48:55 +0000

    Add IsTokenBlacklisted

--- Implementations/TokenRepository.cs
+++ Implementations/TokenRepository.cs
@@ -1,8 +1,12 @@
 namespace ShroomCity.Repositories.Implementations;
+using ShroomCity.Repositories.DbContext;
 using ShroomCity.Repositories.Interfaces;
+using ShroomCity.Utilities.Exceptions;
 
 public class TokenRepository : ITokenRepository
 {
+    private readonly ShroomCityDbContext context;
+    public TokenRepository(ShroomCityDbContext context) => this.context = context;
     public Task BlacklistToken(int tokenId)
     {
         throw new NotImplementedException();
@@ -13,8 +17,9 @@ public class TokenRepository : ITokenRepository
         throw new NotImplementedException();
     }
 
-    public Task<bool> IsTokenBlacklisted(int tokenId)
+    public async Task<bool> IsTokenBlacklisted(int tokenId)
     {
-        throw new NotImplementedException();
+        var token = await this.context.JwtTokens.FindAsync(tokenId) ?? throw new TokenNotFoundException(tokenId);
+        return token.Blacklisted;
     }
 }
