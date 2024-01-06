692378b33fe76e01acd7242f6c7e23391444b0e9
hallgrimurt21 <hallgrimurjth@gmail.com>
2024-01-06 23:17:06 +0000

    Add protected empty constructor for testing purposes

--- DbContext/ShroomCityDbContext.cs
+++ DbContext/ShroomCityDbContext.cs
@@ -5,9 +5,10 @@ using ShroomCity.Repositories.Seed;
 
 public class ShroomCityDbContext : DbContext
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    protected ShroomCityDbContext()
+    {
+    }
     public ShroomCityDbContext(DbContextOptions<ShroomCityDbContext> options) : base(options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
 
     }
